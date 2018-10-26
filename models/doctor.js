ffc35aa329da702c2a36c0eb780e414a4e1f420c
Nicole Barry <nmbarry15@gmail.com>
2018-10-26 13:28:36 -0400

    make doctor and patient models

---
+++ models/doctor.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+    var Doctor = sequelize.define("Doctor", {
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        specialty: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        phone: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            validate: {
+                not: ["[a-z]", "i"],
+                len: 10
+            }
+        },
+        email: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        }
+    });
+    Doctor.associate = function(models) {
+        Doctor.belongsToMany(models.Patient, {
+            through: "PaitentsDoctors",
+            as: "patients",
+            foreignKey: "doctorId",
+            otherKey: "patientId"
+        })
+    }; 
+    return Doctor;
+};
+  
---
+++ models/patient.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, DataTypes) {
+    var Patient = sequelize.define("Patient", {
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        birthday: {
+            allowNull: false,
+            type: DataTypes.DATEONLY
+        },
+        address: {
+            allowNull: false,
+            type: DataTypes.TEXT
+        },
+        phone: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            validate: {
+                not: ["[a-z]", "i"],
+                len: 10
+            }
+        },
+        email: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        }
+    });
+    Patient.associate = function(models) {
+        Patient.belongsToMany(models.Doctor, {
+            through: "DoctorsPatients",
+            as: "doctors",
+            foreignKey: "patientId",
+            otherKey: "doctorId"
+        })
+    }; 
+    return Patient;
+};
+  
