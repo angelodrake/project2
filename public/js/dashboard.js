13a3bc352148469b1814d577a11eca85a0d0471b
Jeremiah Schmutz <Jeremiah.Schmutz@gmail.com>
2018-10-27 13:32:39 -0400

    getting stuff for api

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -14,3 +14,39 @@ $("#phone").on("change keyup paste", function () {
     }
     $("#phone").val(output);
 });
+
+
+function tabChanger(event, tabID) {
+    tabContent = $(".tab-content");
+    for (var i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    };
+
+    tabLinks = $(".tab");
+    for (var i = 0; i < tabLinks.length; i++) {
+        // tabLinks[i].removeClass(" is-active");
+        tabLinks[i].className = tabLinks[i].className.replace(" is-active", "")
+    };
+
+    // $(tabID).
+    document.getElementById(tabID).style.display = "block";
+    event.currentTarget.className += (" is-active");
+    // this.className += (" is-active");
+
+};
+
+// opening the modal by clicking a patient's row
+$(".patient-row").on("click", function () {
+    $("#add-modal").addClass("is-active")
+})
+
+// closing the modal by the cancel button
+$("#add-cancel-button").on("click", function (event) {
+    event.preventDefault();
+    $("#add-modal").removeClass("is-active")
+})
+
+// closing the modal by the x
+$("#add-x").on("click", function (event) {
+    $("#add-modal").removeClass("is-active")
+})
