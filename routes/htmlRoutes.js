545c29a54a8425b7e8639fcd2d59a7ca84cb5964
Angelo Drake <angelodrake@gmail.com>
2018-10-29 12:25:12 -0400

    added functionalty for qr code testing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,4 +5,18 @@ module.exports = function (app) {
   app.get('/index.html', function (req, res) {
     res.render("add-patient")
   });
-};
+
+
+  //router for qr scanner
+  app.get("/patients/:id", function(req, res) {
+    db.Patient.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Doctor, db.Perscription]
+    }).then(function (data) {
+      console.log(data)
+      res.render("patients", data);
+  })
+})
+}
