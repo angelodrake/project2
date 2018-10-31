2bf400eafaeab82f6efcc92232bf233feb63dc8a
Jeremiah Schmutz <Jeremiah.Schmutz@gmail.com>
2018-10-30 23:17:39 -0400

    data animation

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,30 +1,38 @@
 
 
-    var chart = d3.select(".chart")
-    array = []
-    d3.tsv("/api/analytics", function(d) {
-        //change to number
-            d.value = Number(d.value); 
-            array.push(d.value)
-            var maxVal = d3.max(array)       
-            
-           function styleWidth(data) {
-                return Number((data.value/maxVal) * 100) + "%";
-            };
-            var result = styleWidth(d)
+var chart = d3.select(".chart")
+array = []
+var chartWidth = $(".chart").width()
+console.log(chartWidth)
+d3.tsv("/api/analytics", function (d) {
+    //change to number
+    d.value = Number(d.value);
+    array.push(d.value)
+    var maxVal = d3.max(array)
+    function styleWidth(data) {
+        return Number((data.value / maxVal) * 100) + "% ";
+    };
+    var result = styleWidth(d)
 
-            var newDiv = $("<div class ='bar hidden'>")
-            newDiv.attr("style", "width:" + result)
-            newDiv.text(d.name + " ------ " + result)
-            $(".chart").append(newDiv)
-        })
+    var newDiv = $("<div class ='bar hidden' name = 'width" + result + "'>")
+    newDiv.attr("style", "width:" + result + "; position: relative; right: 100%")
+    newDiv.text(d.name + " ------ " + result)
+    $(".chart").append(newDiv)
+})
 
-        function showData() {
-            
-            $(".bar").removeClass("hidden")
-            $(".bar").addClass("show")
-        }
+function showData() {
+    var wide = $(".bar").width() * .62
+    $(".bar").removeClass("hidden")
+    $(".bar").addClass("show")
+    $(".bar").animate({
+        left: "+=" + wide,
+    }, 1000
 
-        $(document).on("click", "#toggle", showData)
-            
+    )
+
+    $("#toggle").addClass("hidden")
+}
+
+$(document).on("click", "#toggle", showData)
+
 
